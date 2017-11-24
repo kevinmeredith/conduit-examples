8c1fedf605b15f1b8a98266f911cfbfc3c162d13
Kevin Meredith <kevin.m.meredith@gmail.com>
2017-11-24 17:29:16 -0500

    Implement peekC and multiplier.

--- my-project/src/Lib.hs
+++ my-project/src/Lib.hs
@@ -8,6 +8,7 @@ import Control.Monad.Trans.List
 import Conduit
 import Control.Applicative
 import Data.MonoTraversable
+import Data.Void (Void)
 
 -- EXERCISE Implement iterMC in terms of mapMC.
 iterMC' :: Monad m => (a -> m ()) -> Conduit a m a
@@ -73,3 +74,53 @@ mapMC' f = loop
          Just x  -> do
            lift $ f x
            loop
+
+-- author: Snoyman
+-- throws away value when not matched!
+myTakeWhileC :: Monad m => (i -> Bool) -> ConduitM i i m ()
+myTakeWhileC f = 
+   loop
+     where
+       loop = do
+         mx <- await
+         case mx of 
+           Nothing -> return ()
+           Just x | f x       -> yield x >> loop
+                  | otherwise -> return ()
+
+myGoodTakeWhileC :: Monad m => (i -> Bool) -> ConduitM i i m ()
+myGoodTakeWhileC f = 
+  loop 
+    where 
+      loop = do
+        mx <- await 
+        case mx of 
+          Nothing -> return ()
+          Just x | f x -> (yield x) >> loop
+                 | otherwise -> leftover x
+
+-- EXERCISE Implement a peek function that gets the next value from upstream, if available, and then puts it back on the stream.
+--peekC :: Monad m => ConduitM a o m (Maybe a)
+peekC' :: Monad m => ConduitM a o m (Maybe a)
+peekC' = do
+  mx <- await
+  case mx of 
+    Nothing -> return Nothing
+    Just x -> (return $ Just x) <* (leftover x)
+
+doubles :: [Double]
+doubles = [1,2,3,4,5,6]
+
+average :: Monad m => ConduitM Double Void m Double
+average = getZipSink (go <$> ZipSink sumC <*> ZipSink lengthC)
+   where
+     go total len = total / fromIntegral len
+
+-- Write a conduit that consumes a stream of Ints. It takes the first Int from the stream, and then multiplies all subsequent Ints by that number and sends them back downstream. You should use the mapC function for this.
+
+multiplier :: Monad m => ConduitM Int Int m ()
+multiplier = do
+  mx <- await
+  case mx of
+    Nothing -> return ()
+    Just x  -> mapC (* x) 
